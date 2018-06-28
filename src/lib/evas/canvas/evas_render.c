7df8918fb2548f8b4bcd3f3e73104a14be5b6bae
Derek Foreman <derekf@osg.samsung.com>
2018-06-28 13:11:59 -0500

    evas_render: Make some code easier to read
    Summary:
    The variable "clean_them" can only ever be EINA_FALSE for much of this
    function, but using it as a return value ensures that anyone not
    intimately familiar with the code will have to read a lot of code
    to figure out that this is so.
    
    Instead, return EINA_FALSE up until the point clean_them can actually
    be something else.
    
    No functional change.
    
    Reviewers: devilhorns, zmike
    
    Reviewed By: zmike
    
    Subscribers: cedric, #committers, zmike
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D6464

--- src/lib/evas/canvas/evas_render.c
+++ src/lib/evas/canvas/evas_render.c
@@ -1748,7 +1748,7 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
      {
         /* don't render if the source is invisible */
         if ((evas_object_is_source_invisible(eo_obj, obj)))
-          return clean_them;
+          return EINA_FALSE;
      }
    else
      proxy_src_clip = proxy_render_data->source_clip;
@@ -1758,7 +1758,7 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
 
    /* leave early if clipper is not visible */
    if ((obj->cur->clipper) && (!obj->cur->clipper->cur->visible))
-     return clean_them;
+     return EINA_FALSE;
 
    eina_evlog("+render_object", eo_obj, 0.0, NULL);
 #ifdef REND_DBG
@@ -1805,7 +1805,7 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
                   RD(level, "  not rendering mask surface\n");
                   RD(level, "}\n");
                   eina_evlog("-render_object", eo_obj, 0.0, NULL);
-                  return clean_them;
+                  return EINA_FALSE;
                }
              // else don't return: draw mask in its surface
           }
@@ -1821,7 +1821,7 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
                        IFRD(!evas_object_is_visible(eo_obj, obj), level, "  not visible\n");
                        RD(level, "}\n");
                        eina_evlog("-render_object", eo_obj, 0.0, NULL);
-                       return clean_them;
+                       return EINA_FALSE;
                     }
                }
              else
@@ -1836,7 +1836,7 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
                        IFRD(!obj->cur->color.a && (obj->cur->render_op == EVAS_RENDER_BLEND), level, "  proxy_src_clip + 0 alpha\n");
                        RD(level, "}\n");
                        eina_evlog("-render_object", eo_obj, 0.0, NULL);
-                       return clean_them;
+                       return EINA_FALSE;
                     }
                   else if (proxy_render_data && (surface != obj->proxy->surface) &&
                            obj->proxy->src_invisible)
@@ -1844,7 +1844,7 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
                        RD(level, "  src_invisible + not proxy surface (recursive proxies)\n");
                        RD(level, "}\n");
                        eina_evlog("-render_object", eo_obj, 0.0, NULL);
-                       return clean_them;
+                       return EINA_FALSE;
                     }
                }
           }
@@ -1855,14 +1855,14 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
              IFRD(obj->clip.clipees || obj->cur->have_clipees, level, "  has clippees\n");
              RD(level, "}\n");
              eina_evlog("-render_object", eo_obj, 0.0, NULL);
-             return clean_them;
+             return EINA_FALSE;
           }
         else if (obj->no_render && (surface != obj->proxy->surface))
           {
              RD(level, "  no_render\n");
              RD(level, "}\n");
              eina_evlog("-render_object", eo_obj, 0.0, NULL);
-             return clean_them;
+             return EINA_FALSE;
           }
      }
    else if (!(((evas_object_is_active(eo_obj, obj) && (!obj->clip.clipees) &&
@@ -1874,7 +1874,7 @@ evas_render_mapped(Evas_Public_Data *evas, Evas_Object *eo_obj,
         IFRD(obj->clip.clipees, level, "  has clippees\n");
         RD(level, "}\n");
         eina_evlog("-render_object", eo_obj, 0.0, NULL);
-        return clean_them;
+        return EINA_FALSE;
      }
 
    // set render_pre - for child objs that may not have gotten it.
