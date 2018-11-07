bc0b8ef62b4d0d8ac836a148095a61679f1eb2af
Marcel Hollerbach <mail@marcel-hollerbach.de>
2018-11-07 17:06:10 +0100

    ci: here comes ninja!
    Differential Revision: https://phab.enlightenment.org/D7255

--- .ci/ci-configure.sh
+++ .ci/ci-configure.sh
@@ -3,80 +3,116 @@
 set -e
 
 . .ci/travis.sh
+if [ "$BUILDSYSTEM" = "ninja" ] ; then
+  if [ "$DISTRO" != "" ] ; then
+    # Normal build test of all targets
+    OPTS=" -Decore-imf-loaders-disabler=scim,ibus -Davahi=false -Dmono=false -Dcxx=false"
 
-CI_BUILD_TYPE="$1"
+    WAYLAND_LINUX_COPTS=" -Dwl=true -Ddrm=true -Dopengl=es-egl"
 
-DEFAULT_LINUX_COPTS="--prefix=/usr/ --with-tests=regular --disable-cxx-bindings --disable-dependency-tracking -C"
+    MISC_LINUX_COPTS=" -Dharfbuzz=true -Dhyphen=true"
 
-WAYLAND_LINUX_COPTS=" --enable-wayland --enable-elput --enable-drm \
---enable-wayland-ivi-shell --enable-gl-drm --with-opengl=es --enable-egl"
+    MISC_DISABLED_LINUX_COPTS=" -Dsystemd=false"
 
-MISC_LINUX_COPTS=" --enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 \
---enable-multisense --enable-lua-old --enable-xpresent --enable-hyphen \
---enable-pixman --enable-pixman-font --enable-pixman-rect --enable-pixman-line \
---enable-pixman-poly --enable-pixman-image --enable-pixman-image-scale-sample \
---enable-image-loader-generic --enable-libuv --enable-tile-rotate --enable-vnc-server \
---enable-fb --enable-v4l2 --enable-cserve \
---enable-ecore-wayland --enable-ecore-drm --enable-cancel-ok --with-crypto=gnutls \
---enable-debug --disable-gstreamer1 --enable-gstreamer"
+    RELEASE_READY_LINUX_COPTS=" --buildtype=release"
 
-MISC_DISABLED_LINUX_COPTS=" --disable-neon --disable-libeeze --disable-systemd --disable-magic-debug \
---disable-valgrind --disable-gstreamer1 \
---disable-fontconfig --disable-fribidi --disable-poppler --disable-spectre --disable-libraw \
---disable-librsvg --disable-xcf --disable-libmount --disable-tslib --disable-audio \
---disable-pulseaudio --disable-avahi --disable-xinput2 --disable-xim --disable-scim \
---disable-ibus --disable-physics --disable-quick-launch --disable-elua"
+    if [ "$1" = "misc" ]; then
+      OPTS="$OPTS $MISC_LINUX_COPTS $WAYLAND_LINUX_COPTS"
+    fi
 
-RELEASE_READY_LINUX_COPTS=" --with-profile=release"
+    if [ "$1" = "misc-disabled" ]; then
+      OPTS="$OPTS $MISC_DISABLED_LINUX_COPTS"
+    fi
 
-patch -p1 < .ci/efl.m4.diff
-sed -i.orig 's/AC_INIT\(.*\)efl_version-[a-zA-Z0-9]\+/AC_INIT\1efl_version/g' configure.ac
+    if [ "$1" = "release-ready" ]; then
+      OPTS="$OPTS $RELEASE_READY_LINUX_COPTS"
+    fi
+    docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
+        --env CXX="ccache g++" --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
+        --env LD="ld.gold" $(cat $HOME/cid) sh -c "mkdir build && meson build $OPTS"
+  else
+    # Prepare OSX env for build
+    mkdir -p ~/Library/LaunchAgents
+    ln -sfv /usr/local/opt/d-bus/*.plist ~/Library/LaunchAgents
+    launchctl load ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
+    export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
 
-if [ "$DISTRO" != "" ] ; then
-  # Normal build test of all targets
-  OPTS="$DEFAULT_LINUX_COPTS"
-
-  if [ "$1" = "misc" ]; then
-    OPTS="$OPTS $MISC_LINUX_COPTS $WAYLAND_LINUX_COPTS"
+    export CFLAGS="-I/usr/local/opt/openssl/include -frewrite-includes $CFLAGS"
+    export LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS"
+    export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+    mkdir build && meson build -Decore-imf-loaders-disabler=scim,ibus -Dx11=false -Davahi=false -Dmono=false -Dcxx=false -Deeze=false -Dsystemd=false -Dnls=false -Dcocoa=true -Demotion-loaders-disabler=gstreamer,gstreamer1,libvlc,xine
   fi
-
-  if [ "$1" = "misc-disabled" ]; then
-    OPTS="$OPTS $MISC_DISABLED_LINUX_COPTS"
-  fi
-
-  if [ "$1" = "release-ready" ]; then
-    OPTS="$OPTS $RELEASE_READY_LINUX_COPTS"
-  fi
-  docker exec $(cat $HOME/cid) sh -c 'rm -f ~/.ccache/ccache.conf'
-  travis_fold autoreconf autoreconf
-  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
-    --env CXX="ccache g++" --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
-    --env LD="ld.gold" $(cat $HOME/cid) sh -c "LIBTOOLIZE_OPTIONS='--no-warn' autoreconf -iv"
-  travis_endfold autoreconf
-  travis_fold configure "configure $OPTS"
-  docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
-    --env CXX="ccache g++" --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
-    --env LD="ld.gold" $(cat $HOME/cid) sh -c ".ci/configure.sh $OPTS"
-  travis_endfold configure
 else
-  OSX_COPTS="--disable-cxx-bindings --with-tests=regular --disable-dependency-tracking -C"
+  CI_BUILD_TYPE="$1"
 
-  # Prepare OSX env for build
-  mkdir -p ~/Library/LaunchAgents
-  ln -sfv /usr/local/opt/d-bus/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
-  export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
+  DEFAULT_LINUX_COPTS="--prefix=/usr/ --with-tests=regular --disable-cxx-bindings --disable-dependency-tracking -C"
 
-  export CFLAGS="-I/usr/local/opt/openssl/include -frewrite-includes $CFLAGS"
-  export LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS"
-  export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+  WAYLAND_LINUX_COPTS=" --enable-wayland --enable-elput --enable-drm \
+  --enable-wayland-ivi-shell --enable-gl-drm --with-opengl=es --enable-egl"
 
-  # Normal build test of all targets
-  rm -f ~/.ccache/ccache.conf
-  travis_fold autoreconf autoreconf
-  LIBTOOLIZE_OPTIONS='--no-warn' autoreconf -iv
-  travis_endfold autoreconf
-  travis_fold configure "configure $OSX_COPTS"
-  .ci/configure.sh $OSX_COPTS
-  travis_endfold configure
+  MISC_LINUX_COPTS=" --enable-harfbuzz --enable-liblz4 --enable-image-loader-webp --enable-xinput22 \
+  --enable-multisense --enable-lua-old --enable-xpresent --enable-hyphen \
+  --enable-pixman --enable-pixman-font --enable-pixman-rect --enable-pixman-line \
+  --enable-pixman-poly --enable-pixman-image --enable-pixman-image-scale-sample \
+  --enable-image-loader-generic --enable-libuv --enable-tile-rotate --enable-vnc-server \
+  --enable-fb --enable-v4l2 --enable-cserve \
+  --enable-ecore-wayland --enable-ecore-drm --enable-cancel-ok --with-crypto=gnutls \
+  --enable-debug --disable-gstreamer1 --enable-gstreamer"
+
+  MISC_DISABLED_LINUX_COPTS=" --disable-neon --disable-libeeze --disable-systemd --disable-magic-debug \
+  --disable-valgrind --disable-gstreamer1 \
+  --disable-fontconfig --disable-fribidi --disable-poppler --disable-spectre --disable-libraw \
+  --disable-librsvg --disable-xcf --disable-libmount --disable-tslib --disable-audio \
+  --disable-pulseaudio --disable-avahi --disable-xinput2 --disable-xim --disable-scim \
+  --disable-ibus --disable-physics --disable-quick-launch --disable-elua"
+
+  RELEASE_READY_LINUX_COPTS=" --with-profile=release"
+
+  patch -p1 < .ci/efl.m4.diff
+  sed -i.orig 's/AC_INIT\(.*\)efl_version-[a-zA-Z0-9]\+/AC_INIT\1efl_version/g' configure.ac
+
+  if [ "$DISTRO" != "" ] ; then
+    # Normal build test of all targets
+    OPTS="$DEFAULT_LINUX_COPTS"
+
+    if [ "$1" = "misc" ]; then
+      OPTS="$OPTS $MISC_LINUX_COPTS $WAYLAND_LINUX_COPTS"
+    fi
+
+    if [ "$1" = "release-ready" ]; then
+      OPTS="$OPTS $RELEASE_READY_LINUX_COPTS"
+    fi
+    docker exec $(cat $HOME/cid) sh -c 'rm -f ~/.ccache/ccache.conf'
+    travis_fold autoreconf autoreconf
+    docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
+      --env CXX="ccache g++" --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
+      --env LD="ld.gold" $(cat $HOME/cid) sh -c "LIBTOOLIZE_OPTIONS='--no-warn' autoreconf -iv"
+    travis_endfold autoreconf
+    travis_fold configure "configure $OPTS"
+    docker exec --env MAKEFLAGS="-j5 -rR" --env EIO_MONITOR_POLL=1 --env CC="ccache gcc" \
+      --env CXX="ccache g++" --env CFLAGS="-fdirectives-only" --env CXXFLAGS="-fdirectives-only" \
+      --env LD="ld.gold" $(cat $HOME/cid) sh -c ".ci/configure.sh $OPTS"
+    travis_endfold configure
+  else
+    OSX_COPTS="--disable-cxx-bindings --with-tests=regular --disable-dependency-tracking -C"
+
+    # Prepare OSX env for build
+    mkdir -p ~/Library/LaunchAgents
+    ln -sfv /usr/local/opt/d-bus/*.plist ~/Library/LaunchAgents
+    launchctl load ~/Library/LaunchAgents/org.freedesktop.dbus-session.plist
+    export PATH="/usr/local/opt/ccache/libexec:$(brew --prefix gettext)/bin:$PATH"
+
+    export CFLAGS="-I/usr/local/opt/openssl/include -frewrite-includes $CFLAGS"
+    export LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS"
+    export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+
+    # Normal build test of all targets
+    rm -f ~/.ccache/ccache.conf
+    travis_fold autoreconf autoreconf
+    LIBTOOLIZE_OPTIONS='--no-warn' autoreconf -iv
+    travis_endfold autoreconf
+    travis_fold configure "configure $OSX_COPTS"
+    .ci/configure.sh $OSX_COPTS
+    travis_endfold configure
+  fi
 fi
