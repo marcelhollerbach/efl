24d4ce600802932a59d9555c7c00a7d9b0a1c0ba
Stefan Schmidt <s.schmidt@samsung.com>
2018-11-07 11:16:52 +0100

    HACK: disbale ewpi rebuild for now

--- .ci/ci-mingw-cross-build.sh
+++ .ci/ci-mingw-cross-build.sh
@@ -11,10 +11,11 @@ export LDFLAGS="-L$PREFIX/lib/"
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/
 
 cd /ewpi/
-sed -i -e 's/installed: yes/installed: no/g' packages/dbus/dbus.ewpi
-sed -i -e 's/installed: yes/installed: no/g' packages/libtiff/libtiff.ewpi
-sed -i -e 's/installed: yes/installed: no/g' packages/*/*.ewpi
-./ewpi /root/EFL/ewpi_64 x86_64-w64-mingw32
+gcc -std=c99 -o ewpi ewpi.c ewpi_map.c
+#sed -i -e 's/installed: yes/installed: no/g' packages/dbus/dbus.ewpi
+#sed -i -e 's/installed: yes/installed: no/g' packages/libtiff/libtiff.ewpi
+#sed -i -e 's/installed: yes/installed: no/g' packages/*/*.ewpi
+#./ewpi /root/EFL/ewpi_64 x86_64-w64-mingw32
 cd /src
 
 # epp is looked for in a hardcoded /usr/local/lib, need to fix in efl
